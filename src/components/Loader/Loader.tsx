9524719c0a5d7cb2ce9827f45b429db5a4f3d0b7
Oleksandr <zombozo223@gmail.com>
2026-06-21 16:33:45 +0300

    add-gallery

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,6 @@
 import style from "./Loader.module.css";
+import { MoonLoader } from "react-spinners";
 
 export default function Loader() {
-  return <div className={style.backdrop}>{/* spinner */}</div>;
+  return <div className={style.backdrop}>{<MoonLoader />}</div>;
 }
